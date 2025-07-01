c48b900b7216b2c0a6b814310cf96b02382d3ffe
Pedro Lucas <148817452+pplucao@users.noreply.github.com>
2025-07-01 18:15:12 -0300

    Update Inimigo.java

--- Inimigo.java
+++ Inimigo.java
@@ -60,8 +60,8 @@ public class Inimigo extends Ponto{ //herda x, y e velocidade
         y += dy * (velocidade);
 
         // Mantém dentro da tela
-        x = Math.max(0, Math.min(x, larguraTela - 80)); // X entre 0 e larguraTela - 20
-        y = Math.max(0, Math.min(y, alturaTela - 80));
+        x = Math.max(0, Math.min(x, larguraTela - 100)); // X entre 0 e larguraTela - 100
+        y = Math.max(0, Math.min(y, alturaTela - 100));
     }
 
     public Rectangle getBounds() { //colisão
